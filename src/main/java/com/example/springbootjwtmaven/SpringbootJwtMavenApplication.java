b6e3ce2ad9e4d33738c9f3c8ab0aae75990fb228
thuongnv <ngo.van.thuong@vsi-international.com>
2022-07-22 11:03:40 +0700

    commit

--- src/main/java/com/example/springbootjwtmaven/SpringbootJwtMavenApplication.java
+++ src/main/java/com/example/springbootjwtmaven/SpringbootJwtMavenApplication.java
@@ -20,7 +20,7 @@ public class SpringbootJwtMavenApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","https://app-demo-ang.netlify.app/"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control,     Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Header")); // this allows all headers
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Request-Allow-Origin", "Access-Control-Allow-Credentials"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
